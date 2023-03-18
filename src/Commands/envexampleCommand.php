4c2ab62da584baa70722f5ca9ad3e2f9dc3ccf67
Simon Jenny <simon.jenny@me.com>
2023-03-18 14:03:59 +0100

    Bugfixes

--- src/Commands/envexampleCommand.php
+++ src/Commands/envexampleCommand.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class envexample extends Command
+class envexampleCommand extends Command
 {
     /**
      * The name and signature of the console command.
